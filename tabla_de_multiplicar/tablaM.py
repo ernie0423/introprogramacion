91320e7365676a24d261a35111522bf17b3be72c
ernie0423 <ernesto.mur@ucb.edu.bo>
2020-10-22 16:24:43 -0400

    tablademultiplicar

---
+++ tabla_de_multiplicar/tablaM.py
@@ -0,0 +1,8 @@
+#tabla de multiplicar
+def tablaM(numI):
+    for i in range(1, 11):
+        print(f"{numI} x{i} = {numI*1}")
+
+for i in range(1, 11):
+    print(f"la tabña de {i} es. -")
+    tablaM(i)
