232e4dca49f033790fc7ce0d7cd06d0729817677
ernie0423 <ernesto.mur@ucb.edu.bo>
2020-11-20 14:43:31 -0400

    ahorcado_actualizado

--- juego_ahorcado/ahorcado_actualizado.py
+++ juego_ahorcado/ahorcado_actualizado.py
@@ -1,117 +1,62 @@
-def imprimirAhorcado(intentos):
-    if(intentos==5):
-        print("================")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("===")
-    if (intentos == 4):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("===")
-    if (intentos == 3):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=            ----")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("===")
-    if (intentos == 2):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=            ----")
-        print("=              |    ")
-        print("=              |    ")
-        print("=              |    ")
-        print("=")
-        print("===")
-    if (intentos == 1):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=            ----")
-        print("=              |    ")
-        print("=              |    ")
-        print("=             /   ")
-        print("=")
-        print("===")
-    if (intentos == 0):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=            ----")
-        print("=              |    ")
-        print("=              |    ")
-        print("=             / \ ")
-        print("=")
-        print("===         PERDISTE!")
+import tkinter as libtk
+from tkinter import *
+def borrarletra():
+    letra.set("")
 
-archivoPalabras=open("palabras.txt","r")
+def reiniciar():
+    lblIntentos.set(6)
+    letra.set("")
+    print("Reiniciar")
 
-listaPalabrasOcultas=archivoPalabras.readlines()
+def buscarLetra():
+    coincidencias = 0
+    l = letra.get()
+    l = l.upper()
+    cintentos = int(lblIntentos.get())
+    for pos in range(0, tam, 1):
+        if (l == vectorPalabraOculta[pos]):
+            vectorPalabraGuiones[pos] = l
+            coincidencias = coincidencias + 1
+    guiones.set(vectorPalabraGuiones)
+    if (coincidencias == 0):
+        cintentos = cintentos - 1
+        lblIntentos.set(cintentos)
+        if (cintentos == 0):
+            guiones.set("PERDISTE, SE ACABARON LOS INTENTOS")
+            print(archivoPalabras)
+    borrarletra()
 
-#palabraoculta=listaPalabrasOcultas[2]
+# Definición palabra oculta y los vectores a utilizar
+archivoPalabras = open("palabraoculta.txt", "r")
+import random
+random.choices(archivoPalabras)
+lista = archivoPalabras.readlines()
 archivoPalabras.close()
-
-
-for palabraoculta in listaPalabrasOcultas:
-
-
-    #palabraoculta=palabraoculta.replace("\n","")
-    palabraoculta = palabraoculta.strip()
-
-    tam=len(palabraoculta)
-    vectorPalabraOculta=[]
-    vectorPalabraGuiones=[]
-    intentos=6
-
-    for pos in range (0,tam,1):
-        vectorPalabraOculta.append(str(palabraoculta[pos]))
-        vectorPalabraGuiones.append("-")
-
-
-    print(vectorPalabraGuiones)
-    while(intentos>0):
-
-        print("Cantidad de intentos: ",intentos)
-        coincidencias=0
-        cantidadguiones=0
-        letra=input("Ingrese una letra: ")
-        letra=letra.upper()
-        for pos in range(0,tam,1):
-            if(letra==vectorPalabraOculta[pos]):
-                vectorPalabraGuiones[pos]=letra
-                coincidencias=coincidencias+1
-
-
-        print(vectorPalabraGuiones)
-
-        for pos in range(0,tam,1):
-            if(vectorPalabraGuiones[pos]=="-"):
-                cantidadguiones=cantidadguiones+1
-
-        if(coincidencias==0):
-            intentos=intentos-1
-            imprimirAhorcado(intentos)
-
-        if(cantidadguiones==0):
-            print("Ganaste, adivinaste la palabra")
-            intentos=-1
-
-
-    if(intentos==0):
-        print("Se terminaron tus intentos!")
+intentos = 6
+palabraoculta = lista[3]
+tam = len(palabraoculta)
+vectorPalabraOculta = []
+vectorPalabraGuiones = []
+for pos in range(0, tam, 1):
+    vectorPalabraOculta.append(str(palabraoculta[pos]))
+    vectorPalabraGuiones.append("-")
+ventana = libtk.Tk()
+ventana.config(width=500, height=500)
+guiones = StringVar()
+etiquetaGuiones = Label(ventana, textvariable=guiones)
+etiquetaGuiones.place(x=125, y=50)
+guiones.set(vectorPalabraGuiones)
+etiquetaIntentosDesc = Label(ventana, text="Intentos: ")
+etiquetaIntentosDesc.place(x=250, y=0)
+lblIntentos = StringVar()
+etiquetaIntentosValor = Label(ventana, textvariable=lblIntentos)
+etiquetaIntentosValor.place(x=300, y=0)
+lblIntentos.set("6")
+letra = StringVar()
+entradaLetra = Entry(ventana, textvariable=letra)
+entradaLetra.place(x=10, y=100)
+botonAdivinar = Button(ventana, text="Adivinar!", command=buscarLetra)
+botonAdivinar.place(x=150, y=95)
+botonReiniciar = Button(ventana, text="Reiniciar!", command=reiniciar)
+botonReiniciar.place(x=150, y=120)
+ventana.mainloop()
