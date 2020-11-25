2d649fece379a9e43fbb79dbbc5a8c984906305d
ernie0423 <ernesto.mur@ucb.edu.bo>
2020-11-25 15:13:06 -0400

    tarea

--- tareas/23/11/tarea.py
+++ tareas/23/11/tarea.py
@@ -1,7 +1,21 @@
 suma=0
 vectorsuma=[]
-vector_1=[20,30,60,8,7]
-vector_2=[18,28,55,5,2]
+vector_1=[]
+vector_2=[]
+
+numeros_1=0
+for i in range(0,5):
+    agregado=int(input("ingrese cinco numeros: "))
+    numeros_1=numeros_1+agregado
+    vector_1.append(numeros_1)
+
+numeros_2=0
+for i in range(0,5):
+    agregado2=int(input("ingrese cinco numeros más: "))
+    numeros_2=numeros_2+agregado2
+    vector_2.append(numeros_2)
+
+
 for num in range(0,5):
     suma = vector_1[num] + vector_2[num]
     vectorsuma.append(suma)
